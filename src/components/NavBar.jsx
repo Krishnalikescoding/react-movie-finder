77bc6e46037222a1c1b1817883d908ae45a8fb07
Krishna <guptakrishnadhananjay@gmail.com>
2026-03-24 20:12:40 +0530

    add burger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
 import "../css/NavBar.css";
+import { useState } from "react";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Movie App</Link>
+        <Link to="/">Listem</Link>
       </div>
-      <div className="navbar-links">
-        <Link to="/" className="nav-link">
+      <div 
+        className="menu-icon" 
+        onClick={() => setMenuOpen(!menuOpen)}   
+      >
+        {menuOpen ? "✕" : "☰"}
+      </div>
+      <div className={`navbar-links ${menuOpen ? "active" : ""}`}>
+        <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>
           Home
         </Link>
-        <Link to="/Favourites" className="nav-link">
+        <Link to="/Favourites" className="nav-link" onClick={() => setMenuOpen(false)}>
           Favourites
         </Link>
-        <Link to="/Watchlist" className="nav-link">
+        <Link to="/Watchlist" className="nav-link" onClick={() => setMenuOpen(false)}>
           Watchlist
         </Link>
       </div>
@@ -22,4 +31,4 @@ function NavBar() {
   );
 }
 
-export default NavBar
+export default NavBar;
